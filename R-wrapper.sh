17d8b3435e35a894399985007bf1d5fc2cb44519
bala <dmbala@gmail.com>
2015-10-21 09:52:55 -0500

    adding Log dir

--- R-wrapper.sh
+++ R-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
- source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
+ source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/current/init/bash
  module load R
  Rscript $1 > mcpi.$2.out
